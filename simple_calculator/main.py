73cb6c7965a556b0c17d28080694c3034d82d173
JichangJang <wlckd90@gmail.com>
2021-06-13 00:39:30 +0900

    Avg lower threshold

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -20,9 +20,12 @@ class SimpleCalculator:
         except ZeroDivisionError:
             return float("inf")
 
-    def avg(self, it, ut=None):
+    def avg(self, it, ut=None, lt=None):
+        if not lt:
+            lt = min(it)
+
         if not ut:
             ut = max(it)
 
-        _it = [x for x in it if x <= ut]
+        _it = [x for x in it if lt <= x <= ut]
         return sum(_it) / len(_it)
